6959eded75d9fd32d70cf4c43daf7ee313172503
Joao Alves <jcmsalves@gmail.com>
2018-05-25 14:05:37 +0100

    initial project setup

---
+++ app/src/main/java/com/jcmsalves/codewarsapi/MembersActivity.kt
@@ -0,0 +1,12 @@
+package com.jcmsalves.codewarsapi
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MembersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_members)
+    }
+}
